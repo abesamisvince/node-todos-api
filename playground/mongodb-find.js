c527deb9125e9e9a7ca410b4d1959007c11c460b
Vincent <vincentabesamis03@gmail.com>
2018-03-21 22:40:59 +0800

    'Init'

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5ab25a6929527713e45c1ce7')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos');
+    // });
+    db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos: ${count}`);
+        
+    }, (err) => {
+        console.log('Unable to fetch todos');
+    });
+
+    db.collection('Users').find({age: 27}).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch users');
+    });
+
+    //db.close(); 
+});
